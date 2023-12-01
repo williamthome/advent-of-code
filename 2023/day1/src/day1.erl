d83cf0172e17c91343370eb1ba045a8906585d50
William Fank Thomé <willegp88@gmail.com>
2023-12-01 18:27:41 -0300

    feat(2023): day 1 part 1

---
+++ 2023/day1/src/day1.erl
@@ -0,0 +1,43 @@
+-module(day1).
+
+-export([ file/1, reduce/1 ]).
+
+-record(state, {current = [], sum = 0}).
+
+-define(EXAMPLE_RESULT, 142).
+-define(INPUT_RESULT, 55488).
+
+file(Filename) ->
+    {ok, Bin} = file:read_file(filename:join([code:priv_dir(day1), Filename])),
+    reduce(Bin).
+
+reduce(Bin) ->
+    reduce(Bin, #state{}).
+
+reduce(<<$\n, Rest/bitstring>>, State) ->
+    Int = to_integer(State#state.current),
+    reduce(Rest, State#state{current = [], sum = State#state.sum + Int});
+reduce(<<H, Rest/bitstring>>, State) when H >= $1, H =< $9 ->
+    reduce(Rest, State#state{current = [H | State#state.current]});
+reduce(<<_, Rest/bitstring>>, State) ->
+    reduce(Rest, State);
+reduce(<<>>, State) ->
+    State#state.sum + to_integer(State#state.current).
+
+to_integer([H]) ->
+    list_to_integer([H, H]);
+to_integer([Last | Rest]) ->
+    [First | _] = lists:reverse(Rest),
+    list_to_integer([First, Last]).
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+example_test() ->
+    ?assertEqual(?EXAMPLE_RESULT, file("example")).
+
+input_test() ->
+    ?assertEqual(?INPUT_RESULT, file("input")).
+
+-endif.
